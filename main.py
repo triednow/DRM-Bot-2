7f964ca5f2e1af784a5f37b0c3587b6160036549
triednow <158821779+triednow@users.noreply.github.com>
2024-08-31 12:55:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Config(object):
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
-    GROUPS = os.environ.get('GROUPS', '-1002075880948').split(',')
+    GROUPS = os.environ.get('GROUPS', '-4505450960').split(',')
     for i in range(len(GROUPS)):
         GROUPS[i] = int(GROUPS[i])
 
